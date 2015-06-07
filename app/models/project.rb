680e818311689558d6cd958a1aef9069ff8316d7
Adam Walker <adamlwalker77@gmail.com>
2015-06-06 22:58:26 -0400

    Made project titles unique

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,6 @@
 class Project < ActiveRecord::Base
-  validates :title, :presence => true
+  validates :title, :presence => true,
+  :uniqueness => { :scope => :title,
+                     :message => 'must be unique' }
   has_many :items, :dependent => :destroy
 end
