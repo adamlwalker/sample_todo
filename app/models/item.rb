ad78be13bac9a740b50e53db7e72989244397056
Adam Walker <adamlwalker77@gmail.com>
2015-06-08 18:23:54 -0400

    minor validation changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ActiveRecord::Base
   validates :action,
     :presence => true,
     :uniqueness => { :scope => :project,
+    				 :unless => -> { !Item.where(:action => self.action, :cleared => nil).where.not(:id => self.id).present?},
                      :message => 'should be unique within a project' }
   validates :project_id, :presence => true
   scope :complete, -> { where(:done => true) }
